4f97af4f8e71aac72819a8e7521d41b3b6ff3a49
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2022-10-26 13:17:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+#The fibonaucci sequence is a series of numbers where each entry is equal to the sum of the previous two
+#ex. 0 1 1 2 3 5 8
+#0 + 1 = 1,  1 + 1 = 2,  1 + 2 = 3,  2 + 3 = 5,  3 + 5 = 8
 
-def fibonaucci(n):
+def fibonaucci(n): 
     if n == 1 or n == 0:
         return n
+    #if the sequence number is one or zero, no calculation is required
+    
     elif n < 0:
         return -1
+    
     return fibonaucci(n-1) + fibonaucci(n-2)
+    #the funciton calls itself to iterate through itself to find the preceding numbers in the sequence
 
 if __name__ == '__main__':
     num = int(input("Fibonaucci: "))
